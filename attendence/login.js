cdf4205b1a6b3d04da42b8d09a49895121ed18df
Kauser Versiani <128464846+Kauser-versiani@users.noreply.github.com>
2023-09-17 10:56:27 -0700

    Add files via upload

--- attendence/login.js
+++ attendence/login.js
@@ -1,26 +1,22 @@
-import { getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import { getDatabase} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 const auth = getAuth();
 const database =getDatabase();
 const login = () => {
-    let email = document.getElementById("login=email");
-    let password = document.getElementById("login-password");
-    signInWithEmailAndPassword(auth, email.value, password.value)
-  .then((resolve) => {
-   
-    alert("Sucessfully login");
-    let userRef = database().ref("user/lucky");
-    userRef.once("value")
-      .then((snapshot) => {
-        let data = snapshot.val();
-        console.log(data);
-      })
-    // ...
-  })
-  .catch((reject) => {
-   alert(reject.message)
-    // ..
-  });
+  let email = document.getElementById("login-email");
+  let password = document.getElementById("login-password");
+  if (email.value == "Kauser7098@gmail.com" && password.value == "Delay$$456") {
+    location.href="attendence.html"
+  }else{
+  signInWithEmailAndPassword(auth, email.value, password.value)
+.then((resolve) => {
+  location.href="student.html"
+  alert("Sucessfully login");
+})
+.catch((reject) => {
+ alert("reject")
+});
+}
 }
 let loginbtn = document.getElementById("login-btn");
 loginbtn.addEventListener("click",login);
